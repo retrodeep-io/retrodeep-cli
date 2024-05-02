984bbf874e4bcc0e1c1eeaa30e95c8d34cd43f0f
ayomideAlaka <muizzyomide@gmail.com>
2024-05-02 20:15:10 +0100

    Fixed indentation issues

--- retrodeep/command/deploy/deploy.py
+++ retrodeep/command/deploy/deploy.py
@@ -491,11 +491,11 @@ def compress_directory(source_dir, output_filename):
             file_path = os.path.join(root, file)
             file_paths.append(file_path)
 
-   with zipfile.ZipFile(f"{output_filename}.zip", 'w', zipfile.ZIP_DEFLATED, allowZip64=True) as zipf:
-        for file_path in file_paths:
-            # Create a relative path for files to keep the directory structure
-            relative_path = os.path.relpath(file_path, source_dir)
-            zipf.write(file_path, relative_path)
+    with zipfile.ZipFile(f"{output_filename}.zip", 'w', zipfile.ZIP_DEFLATED, allowZip64=True) as zipf:
+          for file_path in file_paths:
+              # Create a relative path for files to keep the directory structure
+              relative_path = os.path.relpath(file_path, source_dir)
+              zipf.write(file_path, relative_path)
 
     return f"{output_filename}.zip"
 
